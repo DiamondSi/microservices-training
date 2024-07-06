12113561d82ecfdac2663bacd3617cc7a433f1ca
Dmitry Dubchak <neighborland@outlook.com>
2024-07-06 22:26:10 +0700

    docker run init mappers entities

--- init/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE email_notifier;
-CREATE SCHEMA IF NOT EXISTS email_notifier;
-CREATE TABLE IF NOT EXISTS users
-(
-    user_id int not null,
-    email varchar (40),
-    user_name varchar (256)
-);
